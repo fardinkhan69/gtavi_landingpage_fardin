abb835498484447a708adcf8b5b26fb713d54937
Fardin Khan <fardinofficial69@gmail.com>
2025-09-14 15:47:55 +0600

    done

---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <img src="/images/nav-logo.svg" alt="" className='scale-90' />
+            <img src="/images/menu.svg" alt="" className='w-10' />
+
+        </nav>
+    )
+}
+
+export default NavBar
